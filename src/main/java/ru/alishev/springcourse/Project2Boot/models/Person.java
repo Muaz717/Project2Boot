f53f5910c07e2dc5f686670ff88b3fc22145a3fd
murad <m.azizov03@mail.ru>
2024-12-12 12:20:56 +0300

    First commit

---
+++ src/main/java/ru/alishev/springcourse/Project2Boot/models/Person.java
@@ -0,0 +1,84 @@
+package ru.alishev.springcourse.Project2Boot.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name = "Person")
+public class Person {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
+    private String name;
+
+    @Min(value = 1900, message = "Birth date should be more than 1900")
+    @Max(value = 2025, message = "Birth date should not be less than 2025")
+    @Column(name = "date_of_birth")
+    private int birth;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Book> books;
+
+    public Person() {};
+
+    public Person(String name, int birth) {
+        this.name = name;
+        this.birth = birth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBirth() {
+        return birth;
+    }
+
+    public void setBirth(int birth) {
+        this.birth = birth;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
